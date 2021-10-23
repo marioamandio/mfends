9496f5288f7178a90cc6671569bb5445f7940f18
marioamandio <marioamandio@gmail.com>
2021-10-23 16:54:10 +0100

    initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+// mount function to start up the app
+
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
+
+// if dev and isolation call mount immmediately
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_marketing-dev-root");
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+// We are running through container and we should export the mount function
+export { mount };
